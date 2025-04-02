fc3b26dbd5fe18839dd13d2cbe7597b3cdd6865a
jyheo <jyheo_0@nate.com>
2025-04-02 16:01:15 +0900

    푸터 사용안하는 클래스 삭제

--- components/main/Footer.vue
+++ components/main/Footer.vue
@@ -4,39 +4,38 @@
       <!-- 상단 정보 영역 -->
       <v-row class="mb-6" dense>
         <v-col cols="12" md="4">
-          <div class="text-h6 font-weight-bold mb-2 text-white">회사 정보</div>
-          <div class="text-white text-body-2">
+          <div>
             사업자등록번호 : 503-81-49156<br />
             통신판매업신고 : 제 2016-대구남구-0324 호
           </div>
         </v-col>
         <v-col cols="12" md="4">
-          <div class="text-h6 font-weight-bold mb-2 text-white">Our Contacts</div>
-          <div class="text-white text-body-2">
-            <strong>대구</strong><br />
-            대구광역시 수성구 알파시티1로 31길 2, 4층<br /><br />
-            <strong>서울</strong><br />
+          <div>Our Contacts</div>
+          <div>
+            <strong>대구</strong>
+            대구광역시 수성구 알파시티1로 31길 2, 4층<br />
+            <strong>서울</strong>
             서울특별시 구로구 디지털로26길 111 (제이앤케이디지털타워) 1501호
           </div>
         </v-col>
         <v-col cols="12" md="4">
-          <div class="text-h6 font-weight-bold mb-2 text-white">연락처</div>
-          <div class="text-white text-body-2">
+          <div>연락처</div>
+          <div>
             <strong>Tel</strong> : 053.476.2395 / 053.576.2395<br />
             <strong>Fax</strong> : 053.474.2396 / 053.574.2396
           </div>
         </v-col>
       </v-row>
 
-      <v-divider class="mb-6" color="grey-lighten-1"></v-divider>
+      <v-divider color="grey-lighten-1"></v-divider>
 
       <!-- 하단 링크 및 저작권 -->
       <v-row align="center" justify="space-between">
-        <v-col cols="12" md="6" class="text-white text-body-2">
-          <v-btn variant="text" class="text-white" to="/privacy">개인정보정책</v-btn>
-          <v-btn variant="text" class="text-white" to="/terms">이용약관</v-btn>
+        <v-col cols="12" md="6" >
+          <v-btn variant="text" >개인정보정책</v-btn>
+          <v-btn variant="text" >이용약관</v-btn>
         </v-col>
-        <v-col cols="12" md="6" class="text-md-right text-white text-body-2">
+        <v-col cols="12" md="6" >
           © 2023 copyright EGIS
         </v-col>
       </v-row>
